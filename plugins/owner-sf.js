8c8e90e2715cf58767244da43b5b4f4ad69c32c1
-19MnnZx` <147047037+MannhZx@users.noreply.github.com>
2024-11-21 10:27:31 +0800

    Add files via upload

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -1,20 +1,21 @@
 import fs from 'fs'
-let handler = async (m, {
-	text,
-	usedPrefix,
-	command
-}) => {
-	if (global.xmaze.some(number => m.sender.includes(number))) {
-		if (!text) return;
-		if (!m.quoted.text) throw `balas pesan nya!`
-		let path = `${text}`
-		await fs.writeFileSync(path, m.quoted.text)
-		m.reply(`tersimpan di ${path}`)
-	} else {
-		m.reply('This command is for *R-OWNER* Only')
-	}
+
+// Daftar nomor yang diizinkan
+const allowedNumbers = ['19419318284', '33189313251']
+
+let handler = async (m, { text, usedPrefix, command }) => {
+    // Cek apakah nomor pengirim ada dalam daftar allowedNumbers
+    if (allowedNumbers.includes(m.sender.replace(/[^\d]/g, ''))) {  // Hanya angka yang diperiksa
+        if (!text) return m.reply('Mohon masukkan nama file untuk menyimpan teks.')
+        if (!m.quoted || !m.quoted.text) throw `Balas pesan yang ingin disimpan!`
+        
+        let path = `${text}`
+        await fs.writeFileSync(path, m.quoted.text)
+        m.reply(`Tersimpan di ${path}`)
+    } else {
+        m.reply('Perintah ini hanya untuk nomor tertentu saja.')
+    }
 }
 handler.command = /^(sf)$/i
-handler.rowner = true
 
 export default handler
